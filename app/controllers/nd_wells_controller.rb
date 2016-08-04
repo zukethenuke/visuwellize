1a90f6ad8986a48ae6d9b091bb40e0f10d8c5353
zukethenuke <zukethenuke@hotmail.com>
2016-08-04 13:07:55 -0500

    Added nd index and show pages

--- app/controllers/nd_wells_controller.rb
+++ app/controllers/nd_wells_controller.rb
@@ -1,2 +1,11 @@
 class NdWellsController < ApplicationController
+  def index
+    @wells = NdWell.limit(100)
+    render 'index.html.erb'
+  end
+
+  def show
+    @well = NdWell.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/nd' => 'nd_wells#index'
+  get '/nd/:id' => 'nd_wells#show'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

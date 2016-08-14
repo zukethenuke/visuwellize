35dc2421e410a20f1e684a5610198bc251b4d3f4
zukethenuke <zukethenuke@hotmail.com>
2016-08-14 16:55:45 -0500

    Renamed new method to cum_oil

--- app/models/nd_well.rb
+++ app/models/nd_well.rb
@@ -2,10 +2,10 @@ class NdWell < ActiveRecord::Base
   has_many :nd_monthly_productions
   belongs_to :nd_operator
 
-  def cumulative_oil
+  def cum_oil
     sum = 0
-    nd_monthly_productions.each do |x|
-      sum += x.bbls_oil
+    nd_monthly_productions.each do |row|
+      sum += row.bbls_oil
     end
     sum
   end

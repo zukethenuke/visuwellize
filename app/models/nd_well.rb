b5bbc2268dafeb740eac88db23976332e8e85fb1
zukethenuke <zukethenuke@hotmail.com>
2016-08-14 16:51:21 -0500

    Added method to calculate total well oil and send in json

--- app/models/nd_well.rb
+++ app/models/nd_well.rb
@@ -2,6 +2,14 @@ class NdWell < ActiveRecord::Base
   has_many :nd_monthly_productions
   belongs_to :nd_operator
 
+  def cumulative_oil
+    sum = 0
+    nd_monthly_productions.each do |x|
+      sum += x.bbls_oil
+    end
+    sum
+  end
+
 end
 
 

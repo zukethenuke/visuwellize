684eed2b9ee9debdb4a1dc88e9fe73dbd00efc87
zukethenuke <zukethenuke@hotmail.com>
2016-08-23 14:19:47 -0500

    Added checkboxs to operator list in modal

--- app/controllers/api/nd_wells_controller.rb
+++ app/controllers/api/nd_wells_controller.rb
@@ -1,5 +1,8 @@
 class Api::NdWellsController < ApplicationController
   def index
+    # if params 
+    #   @wells = NdWell.where("operator")
+    # end
     # @wells = NdWell.includes(:nd_monthly_productions).where("cum_oil > ?", 0).limit(100).order("RANDOM()")
     # @wells = NdWell.where("cum_oil > ?", 0).limit(100).order("RANDOM()")
     @wells = NdWell.joins(:nd_operator).where("well_count > ? AND cum_oil > ?", 50, 0).limit(100).order("RANDOM()")

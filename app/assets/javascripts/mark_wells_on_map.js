e0fd297c7d97bac8d735e3a5ee167189be330bdd
zukethenuke <zukethenuke@hotmail.com>
2016-08-29 15:40:45 -0500

    Ploted well locations on map

--- app/assets/javascripts/mark_wells_on_map.js
+++ app/assets/javascripts/mark_wells_on_map.js
@@ -1,51 +1,7 @@
 
 function initMap(wells) {
+  console.log('map',wells);
 
-  // var addGeoJsonToWells = function(wells) {
-  //   wells.map(function(well) {
-  //     well.geojson = {
-  //       type: "Feature",
-  //       properties: {
-  //         operator: well.operator,
-  //         depth: well.depth
-  //         cumOil: 
-  //       }
-  //     };
-  //   });
-  // };
-
-  var markers = [
-    {
-      coords: {lat: 52.6663, lng: 0.1588},
-      info: "Wisbech"
-    },
-    {
-      coords: {lat: 51.1279, lng: 1.3134},
-      info: "Dover"
-    },
-    {
-      coords: {lat: 49.7150, lng: -2.1974},
-      info: "Alderney"
-    },
-    {
-      coords: {lat: 28.2916, lng: -16.6291},
-      info: "Tenerife"
-    },
-    {
-      coords: {lat: 14.6415, lng: -61.0242},
-      info: "Martinique"
-    },
-    {
-      coords: {lat: 12.1696, lng: -68.9900},
-      info: "Curacau"
-    },
-    {
-      coords: {lat: 16.3298, lng: -86.5300},
-      info: "Roatan"
-    }
-  ];
-
-  
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
     minZoom: 7,
@@ -57,22 +13,19 @@ function initMap(wells) {
   new google.maps.LatLng(50, -95) 
   );
 
-
-
-  markers.forEach(function(d) {
+  wells.forEach(function(well) {
     var marker = new google.maps.Marker({
-      position: d.coords,
+      position: {lat: well.latitude, lng: well.longitude},
       map: map,
-      title: d.info
+      title: well.operator
     });
 
     var infowindow = new google.maps.InfoWindow({
-      content: d.info
+      content: well.operator
     });
 
     marker.addListener('click', function() {
       infowindow.open(map, marker);
     });
   });
-
 }

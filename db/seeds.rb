daf911107a1858debbabe0f92d0b011de10e63c1
zukethenuke <zukethenuke@hotmail.com>
2016-09-11 15:46:12 -0500

    seed wells

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,24 @@
 
 require 'csv'
 
-# csv_file = File.open(Rails.root.join('lib', 'seeds', 'WellIndex_reduced_for_database.csv'))
-# CSV.foreach(csv_file, :headers => true, :header_converters => :symbol) do |row|
-#   p row[:file_no]
-#   NdWell.create(
-#     api_no: row[:api_no],
-#     file_no: row[:file_no],
-#     operator: row[:operator],
-#     well_name: row[:well_name],
-#     spud_date: row[:spud_date],
-#     td: row[:td],
-#     field_name: row[:field_name],
-#     latitude: row[:latitude],
-#     longitude: row[:longitude],
-#     well_type: row[:well_type],
-#     well_status: row[:well_status],
-#     cum_oil: row[:cum_oil],
-#   )
-# end
+csv_file = File.open(Rails.root.join('lib', 'seeds', 'WellIndex_reduced_for_database.csv'))
+CSV.foreach(csv_file, :headers => true, :header_converters => :symbol) do |row|
+  p row[:file_no]
+  NdWell.create(
+    api_no: row[:api_no],
+    file_no: row[:file_no],
+    operator: row[:operator],
+    well_name: row[:well_name],
+    spud_date: row[:spud_date],
+    td: row[:td],
+    field_name: row[:field_name],
+    latitude: row[:latitude],
+    longitude: row[:longitude],
+    well_type: row[:well_type],
+    well_status: row[:well_status],
+    cum_oil: row[:cum_oil]
+  )
+end
 
 # hashes = []  # peter
 # csv_file = File.open(Rails.root.join('lib', 'seeds', 'monthly_production_info_reduced_for_database.csv'))

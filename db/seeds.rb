98b528be47a50c4cef0b907b298957eb0c4de940
zukethenuke <zukethenuke@hotmail.com>
2016-09-11 17:41:49 -0500

    Reseed wells

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,39 +8,39 @@
 
 require 'csv'
 
-# csv_file = File.open(Rails.root.join('lib', 'seeds', 'WellIndex_reduced_for_database.csv'))
-# CSV.foreach(csv_file, :headers => true, :header_converters => :symbol) do |row|
-#   p row[:file_no]
-#   NdWell.create(
-#     api_no: row[:api_no],
-#     file_no: row[:file_no],
-#     operator: row[:operator],
-#     well_name: row[:well_name],
-#     spud_date: row[:spud_date],
-#     td: row[:td],
-#     field_name: row[:field_name],
-#     latitude: row[:latitude],
-#     longitude: row[:longitude],
-#     well_type: row[:well_type],
-#     well_status: row[:well_status],
-#     cum_oil: row[:cum_oil]
-#   )
-  
-# end
-
-hashes = []  # peter
-csv_file = File.open(Rails.root.join('lib', 'seeds', 'monthly_production_info_reduced_for_database.csv'))
+csv_file = File.open(Rails.root.join('lib', 'seeds', 'WellIndex_reduced_for_database.csv'))
 CSV.foreach(csv_file, :headers => true, :header_converters => :symbol) do |row|
-  p row[:nd_well_id]
-  
-  NdMonthlyProduction.create(
-    nd_well_id: row[:nd_well_id],
-    pool: row[:pool],
-    date: row[:date],
-    bbls_oil: row[:bbls_oil],
-    bbls_water: row[:bbls_water],
-    mcf_prod: row[:mcf_prod]
+  p row[:file_no]
+  NdWell.create(
+    api_no: row[:api_no],
+    file_no: row[:file_no],
+    operator: row[:operator],
+    well_name: row[:well_name],
+    spud_date: row[:spud_date],
+    td: row[:td],
+    field_name: row[:field_name],
+    latitude: row[:latitude],
+    longitude: row[:longitude],
+    well_type: row[:well_type],
+    well_status: row[:well_status],
+    cum_oil: row[:cum_oil]
   )
+  
+end
+
+# hashes = []  # peter
+# csv_file = File.open(Rails.root.join('lib', 'seeds', 'monthly_production_info_reduced_for_database.csv'))
+# CSV.foreach(csv_file, :headers => true, :header_converters => :symbol) do |row|
+#   p row[:nd_well_id]
+  
+#   NdMonthlyProduction.create(
+#     nd_well_id: row[:nd_well_id],
+#     pool: row[:pool],
+#     date: row[:date],
+#     bbls_oil: row[:bbls_oil],
+#     bbls_water: row[:bbls_water],
+#     mcf_prod: row[:mcf_prod]
+#   )
 
 #   hashes << {   # peter, seed everything at once
 #     nd_well_id: row[:nd_well_id],
@@ -50,7 +50,7 @@ CSV.foreach(csv_file, :headers => true, :header_converters => :symbol) do |row|
 #     bbls_water: row[:bbls_water],
 #     mcf_prod: row[:mcf_prod]
 #   }
-end
+# end
 # NdMonthlyProduction.create(hashes) # peter
 
 # ------------------------------------------------------------------

f49618721f0141344893b94ea64be02c71469b24
zukethenuke <zukethenuke@hotmail.com>
2016-09-12 14:18:35 -0500

    removed some from monthly seed file 10 and add readme modals

--- app/assets/javascripts/ScatterCtrl.js
+++ app/assets/javascripts/ScatterCtrl.js
@@ -111,6 +111,9 @@ window.requestInterval = function(fn, delay) {
             scatter(response.data, map, mapMarkers);
           }, function(errors) {
           });
+          if (yearIndex === 0) {
+            isPaused = true; //pause after readme modal opens
+          }
           if (years[yearIndex] < 2000) {
             yearIndex += 5;
           }else {
@@ -124,6 +127,20 @@ window.requestInterval = function(fn, delay) {
         }
       }, 2000, isPaused);
     };
+
+    $(document).ready(function(){
+        // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        starting_top: '4%', // Starting top style attribute
+        ending_top: '10%' // Ending top style attribute
+        // complete: function() { $scope.scatterOpeningAnimation(); }
+      });
+      $('#readMe').openModal();
+    });
 
    
     $('.modal-trigger').leanModal({
